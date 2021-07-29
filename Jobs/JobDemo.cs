532089ee97e7db49c3d6fc6fa81583118fd2d5c2
Tafadzwa Manyenga <30636573+TafadzwaManyenga@users.noreply.github.com>
2021-07-30 02:45:27 +1000

    Created Job and JobDemo classes. Basic objects that handle customer and job information.

---
+++ Jobs/JobDemo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace JobDemo
+{
+    class Program {
+        
+
+        static void Main(string[] args)
+        {
+            Job[] a = new Job[5];
+            int counter = 0;
+
+            foreach (Job x in a)
+            {
+                Console.Write("Job number: ");
+                string v = Console.ReadLine();
+                a[counter].myJobNumber = int.Parse(v);
+
+                Console.Write("Customer Name: ");
+                a[counter].myCustomerName = v;
+
+                Console.Write("Description: ");
+                a[counter].myDescription = v;
+
+                Console.Write("Estimated Hours: ");
+                a[counter].myEstimatedHours = Int32.Parse(v);
+
+                counter++;
+            }
+
+            Console.WriteLine(a[0].myDescription.ToString());
+        }
+    }
+}
---
+++ Jobs/Jobs.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace JobDemo
+{
+    public class Job {
+        //The class members are here.
+        protected string customerName;
+        protected int jobNumber;
+        protected string description;
+        protected int estimatedHours;
+        protected float price;
+
+        private int n;
+
+
+        public int myJobNumber
+        {
+            get { return jobNumber; }
+            set { jobNumber = value; }
+        }
+        public string myCustomerName
+        {
+            get { return customerName; }
+            set { customerName = value; }
+        }
+        public string myDescription
+        {
+            get { return description; }
+            set { description = value; }
+        }
+        public int myEstimatedHours
+        {
+            get { return estimatedHours; }
+            set { estimatedHours = value; }
+        }
+        //Constructor
+        public Job() {
+            //Parameters
+            jobNumber = 0;
+            customerName = "";
+            description = "";
+            estimatedHours = 0;
+        }
+
+        //3.1
+        public bool Equals(int job1, int job2) {
+            bool equalJobs = false;
+
+            //Determine if two jobs are equal.
+            if (job1 == job2) {
+                equalJobs = true;
+            }
+
+            return equalJobs;
+        }
+
+        //3.2
+        public override int GetHashCode() {
+            return n;//Not meant to be 0
+        }
+
+        //3.3
+        public override string ToString()
+        {
+            return n.ToString();
+        }
+    }
+}
